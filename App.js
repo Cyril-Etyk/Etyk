5337f511a23bbd68f99d3e416f2c6e204ab16965
Cyril <cyril.wastchenko@gmail.com>
2020-07-27 15:48:26 +0200

    Setup Screen Tickets
    TicketCard trial

--- App.js
+++ App.js
@@ -1,12 +1,28 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 
+import TicketCard from "./components/TicketCard";
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text> Open up App.js to start working on your TEST12! </Text>
-    </View>
+    <TicketCard style={styles.inputContainer}>
+      <Text>Test</Text>
+      <Text>Test</Text>
+      <Text>Test</Text>
+      <Text>Test</Text>
+      <Text>Test</Text>
+    </TicketCard>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  inputContainer: {
+    width: "95%",
+    maxWidth: "95%",
+    marginHorizontal: "2.5%",
+    alignItems: "center",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginTop: "25%",
+  },
+});
